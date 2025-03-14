940719d0f3e8166a9e880274686d494f9ecf232c
dan-mutua <danmutua15@gmail.com>
2025-03-14 12:56:55 +0300

    add @nestjs/config and configure Mongoose connection in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TaskModule } from './task/task.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TasksModule } from './task/task.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TaskModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      process.env.MONGO_URI || 'mongodb://localhost:27017/todo-app',
+    ),
+    TasksModule,
+  ],
 })
 export class AppModule {}
